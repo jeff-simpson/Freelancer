40d3744c8e2b3b42b73ba5f9ad8ee3fa7cb12fb1
Jeff Simpson <jeffsimpson199@gmail.com>
2017-03-24 11:36:21 -0400

    updated persistence layer

--- src/edu/uga/cs4300/persistlayer/FreelancerPersistImpl.java
+++ src/edu/uga/cs4300/persistlayer/FreelancerPersistImpl.java
@@ -135,7 +135,7 @@ public class FreelancerPersistImpl {
 	public int assignTask(User user, Task task) throws SQLException
 	{
 		
-		String query ="INSERT INTO asignee" +
+		String query ="INSERT INTO asignee " +
 					"(task_id, performer_id) VALUES" +
 					"("+ task.getId() + ","+ user.getId()+");";
 				
@@ -149,7 +149,7 @@ public class FreelancerPersistImpl {
 	public int completeTask(Task task) throws SQLException
 	{
 		
-		String query ="INSERT INTO completedTasks" +
+		String query ="INSERT INTO completedTasks " +
 					"(task_id) VALUES" +
 					"("+ task.getId()+");";
 				
@@ -159,200 +159,58 @@ public class FreelancerPersistImpl {
 	
 	//Transactions
 	
-	public int addTransaction(User creater, User performer, Task task, double Amount) throws SQLException
+	public int addTransaction(User creator, User performer, Task task, double amount) throws SQLException
 	{
 		
-		String query ="INSERT INTO asignee" +
-					"(task_id, performer_id) VALUES" +
-					"("+ task.getId() + ","+ user.getId()+");";
+		String query ="INSERT INTO transactions " +
+					"(creator_id, performer_id ,task_id, amount) VALUES" +
+					"("+ creator.getId()+ ","+ performer.getId()+ task.getId() + ","+ amount +");";
 				
 				
 		return DbAccessInterface.create(query);	
 	}
 	
-	public static int updateGenre(Movie movie, String genre) throws SQLException
+	
+	public int deleteTransaction(User creator, Task task, double amount) throws SQLException
 	{
 		
-		String query ="UPDATE movies_genres " +
-					"SET genre = '" + genre +
-					"' WHERE movie_id = "+ movie.getId()+";";
+		String query ="DELETE FROM transactions" +
+					" WHERE creator_id = "+ creator.getId()+
+					" and task_id = "+ task.getId() +
+					 " and amount = "+ amount +";";
+				
+				
+		return DbAccessInterface.create(query);	
+	}
+	
+	
+	// Account Balance
+	
+	public static ResultSet returnAccountBalance(User user) throws SQLException
+	{
+		
+		
+		String query = "SELECT * FROM accountBalance "+
+						"WHERE user_id ="+ user.getId() +";";
+		
+		ResultSet rs = DbAccessInterface.retrieve(query);
+		
+		return rs;
+		
+	}
+	
+	public static int updateAccountBalance(User user, double balance) throws SQLException
+	{
+		
+		String query ="UPDATE accountBalance " +
+					"SET balance = " + balance +
+					" WHERE user_id = "+ user.getId()+";";
 					
 								
 		return DbAccessInterface.create(query);
 		
 	}
 	
-	public static int deleteMovie(int movie_id) throws SQLException
-	{
-		
-		String query ="DELETE FROM movies " +
-					  "WHERE id = "+ movie_id +";";
-					
-		
-		return DbAccessInterface.delete(query);
-		
-		
-	}
-	
-	public static int deleteMovieGenreLink(int movie_id) throws SQLException
-	{
-		
-		String query ="DELETE FROM movies_genres " +
-					  "WHERE movie_id = "+ movie_id +";";
-					
-		
-		return DbAccessInterface.delete(query);
-		
-		
-	}
-	
-	public static int addReview(int movie_id, String review) throws SQLException
-	{
-		
-		String query ="INSERT INTO reviews " +
-					"(movie_id, review) VALUES " +
-					"("+ movie_id + ", '" + review +"' );";
-		return DbAccessInterface.create(query);
-		
-	}
-	
-	
-	
-	public static int addMovie(String title, int year, double rank) throws SQLException
-	{
-		
-		String query ="INSERT INTO movies " +
-					"(name, year, rank) VALUES " +
-					"('"+ title + "', " + year +", "+ rank+ ");";
-		return DbAccessInterface.create(query);
-		
-	}
-	
-	public static int addGenreToMovie(int movie_id, String genre) throws SQLException
-	{
-		
-		String query ="INSERT INTO movies_genres " +
-					"(movie_id, genre) VALUES " +
-					"("+ movie_id + ", '" + genre +"');";
-		return DbAccessInterface.create(query);
-		
-	}
-	
 
-	public int updateReview(Review review, Movie movie) throws SQLException
-	{
-		
-		String query ="UPDATE reviews" +
-					" SET movie_id = '" + review.getMovie_id()  + "', review = '" +review.getReview()+
-					"'WHERE id = '"+ review.getId()+"';";
-					
-				
-		return DbAccessInterface.create(query);
-		
-	}
-	
-	public int deleteReview(Review review) throws SQLException
-	{
-		
-		String query ="DELETE FROM reviews " +
-					"WHERE id = "+ review.getId() +";";
-					
-		return DbAccessInterface.delete(query);
-		
-	}
-	public static int deleteReviewsByMovie(int movie_id) throws SQLException
-	{
-		
-		String query ="DELETE FROM reviews " +
-					"WHERE movie_id = "+movie_id +";";
-					
-		return DbAccessInterface.delete(query);
-		
-	}
-	
-	public static ResultSet returnMovieViaGenre(String genre) throws SQLException
-	{
-		
-		
-		String query = "SELECT movies.id, movies.name, movies.year, movies.rank from movies, movies_genres "+
-						"WHERE movies_genres.movie_id = movies.id "+
-						"and movies_genres.genre ='"+ genre +"';";
-		
-		ResultSet rs = DbAccessInterface.retrieve(query);
-		
-		return rs;
-		
-	}
-	
-
-	public static ResultSet returnMovieViaTitle(String title) throws SQLException
-	{
-		
-		
-		String query = "SELECT movies.id, movies.name, movies.year, movies.rank "+
-						"from movies "+
-						"WHERE movies.name  ='"+ title +"' limit 1;";
-		
-		ResultSet rs = DbAccessInterface.retrieve(query);
-		
-		return rs;
-		
-	}
-	
-	public static ResultSet returnMovieViaID(int movie_id) throws SQLException
-	{
-		
-		
-		String query = "SELECT movies.id, movies.name, movies.year, movies.rank "+
-						"from movies "+
-						"WHERE movies.id  ='"+ movie_id +"';";
-		
-		ResultSet rs = DbAccessInterface.retrieve(query);
-		
-		return rs;
-		
-	}
-	
-	
-	public static ResultSet getGenreOfMovie(Movie movie) throws SQLException
-	{
-		
-		
-		String query = "SELECT genre "+
-						"from movies_genres "+
-						"WHERE movie_id ="+ movie.getId() +" ORDER BY genre;";
-		
-		ResultSet rs = DbAccessInterface.retrieve(query);
-		
-		return rs;
-		
-	}
-	
-	public static ResultSet returnReviews(Movie movie) throws SQLException
-	{
-		
-		
-		String query = "select * from reviews where movie_id = "+ movie.getId() +";";
-		
-		ResultSet rs = DbAccessInterface.retrieve(query);
-		
-		return rs;
-		
-	}
-	
-	
-	
-	
-	public static void deleteReviewByID(int id) throws SQLException
-	{
-		String query = "Delete from reviews where id = "+ id +";";
-		
-		DbAccessInterface.delete(query);
-		
-	}
-	
-
-	
-	
 
 }
