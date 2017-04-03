a4b0a4483aa87c5c705864c1c72040b7ba3d0fd9
Ivano Milo <ivanomilo@s172-20-164-h206.paws.uga.edu>
2017-04-03 11:57:33 -0400

    added servlet functions to redirect to templates (just to welcome when login at this point because no other pages exist yet  -I

--- src/edu/uga/cs4300/boundary/FreelancerBoundary.java
+++ src/edu/uga/cs4300/boundary/FreelancerBoundary.java
@@ -1,15 +1,10 @@
 package edu.uga.cs4300.boundary;
 
+
+
 import java.io.IOException;
 import java.io.Writer;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,9 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import edu.uga.cs4300.logiclayer.MovieLogicImpl;
-import edu.uga.cs4300.objectlayer.Movie;
-import edu.uga.cs4300.objectlayer.Review;
+import com.mysql.jdbc.Connection;
+
 import edu.uga.cs4300.persistlayer.DatabaseAccess;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapperBuilder;
@@ -29,13 +23,13 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
 
-@WebServlet("/IMDBBoundary")
+@WebServlet("/FreelancerBoundary")
 public class FreelancerBoundary extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
 
     Configuration cfg = null;
-    static Connection con = DatabaseAccess.connect();
+    static Connection con = (Connection) DatabaseAccess.connect();
 
     private String templateDir = "/WEB-INF/templates";
 
@@ -51,352 +45,15 @@ public class FreelancerBoundary extends HttpServlet
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
 
     }
-       
 
-    
-    
-    /** 
-     * This method checks to see if a value was entered
-     */
-    public static boolean empty( final String s )
-    {
-    	  return s == null || s.trim().isEmpty();
-    }
-    
-
-    /** 
-     * Because of the checkbox implementation, this mehtod handles calling all of the requests from the checkboxes
-     * returns an ArrayList of all the genres that were submitted
-     */
-    public static ArrayList<String> getSubmittedGenres(HttpServletRequest request)
-    {
-    	ArrayList < String > genresSort = new ArrayList < String > ();
-    	ArrayList < String > genres = new ArrayList < String > ();
-    	
-    	String Action = request.getParameter("Action");
-    	String Animation = request.getParameter("Animation");
-    	String Comedy  = request.getParameter("Comedy");
-    	String Crime = request.getParameter("Crime");
-    	String Drama  = request.getParameter("Drama");
-    	String Family  = request.getParameter("Family");
-    	String Fantasy = request.getParameter("Fantasy");
-    	String Horror  = request.getParameter("Horror");
-    	String Music = request.getParameter("Music");
-    	String Musical = request.getParameter("Musical");
-    	String Romance = request.getParameter("Romance");
-    	String SciFi  = request.getParameter("Sci-Fi");
-    	String Thriller = request.getParameter("Thriller");
-    	String War  = request.getParameter("War");
-    	
-    	genresSort.add(Action);
-		genresSort.add(Animation);
-		genresSort.add(Comedy);
-		genresSort.add(Crime); 
-		genresSort.add(Drama); 
-		genresSort.add(Family); 
-		genresSort.add(Fantasy); 
-		genresSort.add(Horror); 
-		genresSort.add(Music); 
-		genresSort.add(Musical); 
-		genresSort.add(Romance); 
-		genresSort.add(SciFi); 
-		genresSort.add(Thriller); 
-		genresSort.add(War); 
-
-
-		for (int i = 0; i < genresSort.size(); i++)
-		{
-			if(!empty(genresSort.get(i)))
-			{
-				genres.add(genresSort.get(i));
-				System.out.println(genresSort.get(i));
-			}
-			
-		}
-		
-		return genres;
-    }
-    /** 
-     * A basic method that just stores all the available methods, used to clean up code
-     */
-    public static ArrayList<String> getAllGenres()
-    {
-    	
-    	ArrayList < String > genres = new ArrayList < String > ();
-    	
-    	String Action ="Action";
-    	String Animation ="Animation";
-    	String Comedy  = "Comedy";
-    	String Crime = "Crime";
-    	String Drama  = "Drama";
-    	String Family  = "Family";
-    	String Fantasy = "Fantasy";
-    	String Horror  = "Horror";
-    	String Music ="Music";
-    	String Musical = "Musical";
-    	String Romance = "Romance";
-    	String SciFi  ="Sci-Fi";
-    	String Thriller = "Thriller";
-    	String War  = "War";
-    	
-    	genres.add(Action);
-    	genres.add(Animation);
-    	genres.add(Comedy);
-    	genres.add(Crime); 
-    	genres.add(Drama); 
-    	genres.add(Family); 
-    	genres.add(Fantasy); 
-    	genres.add(Horror); 
-    	genres.add(Music); 
-    	genres.add(Musical); 
-    	genres.add(Romance); 
-    	genres.add(SciFi); 
-    	genres.add(Thriller); 
-    	genres.add(War); 
-    	
-    	return genres;
-    	
-    }
-
-
-    public void runTemplate(HttpServletRequest request, HttpServletResponse response) throws SQLException
+    public void runTemplate(HttpServletRequest request, HttpServletResponse response, String name) throws SQLException
     {
         // You can use this structure for all of your objects to be sent to browser
         Template template = null;
         DefaultObjectWrapperBuilder df = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);
         SimpleHash root = new SimpleHash(df.build());
-        String templateName = "";
+        String templateName = name + ".ftl";
         
-        
-        /** 
-         * Is active when the user is searching for all movies containing a genre
-         */
-        
-        if(request.getParameterMap().containsKey("movieGenre"))
-        {
-        	String genre = request.getParameter("movieGenre");
-        	
-        	ArrayList < Movie > movies = new ArrayList < Movie > ();
-        	movies = MovieLogicImpl.returnMovieViaGenre(genre);
-        	
-        	root.put("movies", movies);
-        	templateName = "genre.ftl";
-        
-        }
-        
-        /** 
-         * Is active when a user searches for a movie by name
-         */
-        
-        if(request.getParameterMap().containsKey("movieName"))
-        {
-        	String title = request.getParameter("movieName");
-        	
-        	ArrayList < Review > reviews = new ArrayList < Review > ();
-        	Movie movie = MovieLogicImpl.returnMovieViaTitle(title);
-        	
-        	ArrayList < String> genres = MovieLogicImpl.getGenreOfMovie(movie);
-        	
-        	if(movie.getName() == null)
-        	{
-        		root.put("movieTitle", title);
-        		templateName ="createMovie.ftl";
-        		
-        	}
-        	else
-        	{
-        		reviews = MovieLogicImpl.returnReviews(movie);
-            	
-            	root.put("movie", movie);
-            	root.put("genres", genres);
-            	root.put("reviews", reviews);
-            	templateName = "reviews.ftl";
-        	}
-        		
-        
-        }
-        
-        /** 
-         * is active when user attempts to delete a review
-         */
-        
-        if(request.getParameterMap().containsKey("deleteReview"))
-        {
-        	String reviewID = request.getParameter("reviewID");
-        	String title = request.getParameter("movieName");
-        	
-        	int id = Integer.parseInt(reviewID);
-        	MovieLogicImpl.deleteReview(id);
-        	
-        	ArrayList < Review > reviews = new ArrayList < Review > ();
-        	Movie movie = MovieLogicImpl.returnMovieViaTitle(title);
-        	reviews = MovieLogicImpl.returnReviews(movie);
-        	
-        	ArrayList < String> genres = MovieLogicImpl.getGenreOfMovie(movie);
-        	
-        	root.put("genres", genres);
-        	root.put("movie", movie);
-        	root.put("reviews", reviews);
-        	templateName = "reviews.ftl";
-        
-        }
-        
-        
-        /** 
-         * is actiive when user wants to write a review
-         */
-        if(request.getParameterMap().containsKey("createReview"))
-        {
-        	String review = request.getParameter("review");
-        	String movieID = request.getParameter("movieID");
-        	
-        	int id = Integer.parseInt(movieID);
-        	MovieLogicImpl.addReview(id, review);
-        	
-        	ArrayList < Review > reviews = new ArrayList < Review > ();
-        	Movie movie = MovieLogicImpl.returnMovieViaID(id);
-        	reviews = MovieLogicImpl.returnReviews(movie);
-        	
-        	ArrayList < String> newGenres = MovieLogicImpl.getGenreOfMovie(movie);
-        	
-        	root.put("genres", newGenres);
-        	root.put("movie", movie);
-        	root.put("reviews", reviews);
-        	templateName = "reviews.ftl";
-        
-        }
-        
-        /** 
-         * is active when a user wants to create a movie
-         */
-        
-        if(request.getParameterMap().containsKey("createMovie"))
-        {
-        	String title = request.getParameter("title");
-        	String year = request.getParameter("year");
-        	String rank = request.getParameter("rank");
-       /// 	String genre = request.getParameter("movieGenre");
-        	
-        	// Get all genres
-
-        	ArrayList < String > genres = getSubmittedGenres(request);
-
-        	if( !empty(title) && !empty(year) && !empty(rank) && !genres.isEmpty())
-        	{
-        		int movie_year = Integer.parseInt(year);
-            	double movie_rank = Double.parseDouble(rank);
-            	
-            	MovieLogicImpl.addMovie(title, movie_year, movie_rank, genres);
-            	
-            	ArrayList < Review > reviews = new ArrayList < Review > ();
-            	Movie movie = MovieLogicImpl.returnMovieViaTitle(title);
-            	reviews = MovieLogicImpl.returnReviews(movie);
-            	
-            	ArrayList < String> newGenres = MovieLogicImpl.getGenreOfMovie(movie);
-            	
-            	root.put("genres", newGenres);
-            	root.put("movie", movie);
-            	root.put("reviews", reviews);
-            	templateName = "reviews.ftl";
-        		
-        	}
-        	else
-        	{
-        		
-        		String Error = "You must have fill out the correct fields";
-        		templateName = "createMovie.ftl";
-            	root.put("error", Error);
-        	}
-        	
-        
-        }
-        
-        /** 
-         * Is active when a user wants to update a movie
-         */
-        if(request.getParameterMap().containsKey("manageMovie"))
-        {
-        	
-        	String movieID = request.getParameter("movieID");
-        	int id = Integer.parseInt(movieID);
-
-        	Movie movie = MovieLogicImpl.returnMovieViaID(id);
-
-        	ArrayList<String> allGenres = getAllGenres();
-        	ArrayList<String> movieGenres = MovieLogicImpl.getGenreOfMovie(movie);
-        	
-        	root.put("allGenres", allGenres);
-        	root.put("movieGenres", movieGenres);
-        	root.put("movie", movie);
-        	templateName = "manageMovie.ftl";
-        
-        }
-        
-        
-        if(request.getParameterMap().containsKey("updateMovie"))
-        {
-        	String id = request.getParameter("movieID");
-        	String title = request.getParameter("title");
-        	String year = request.getParameter("year");
-        	String rank = request.getParameter("rank");
-        	//String genre = request.getParameter("movieGenre");
-        	
-        	int movie_id = Integer.parseInt(id);
-        	
-        	ArrayList < String > genres = getSubmittedGenres(request);
-        	
-        	if( !empty(title) && !empty(year) && !empty(rank) && !genres.isEmpty()) 		
-        	{
-        		
-            	int movie_year = Integer.parseInt(year);
-            	double movie_rank = Double.parseDouble(rank);
-            	
-            	MovieLogicImpl.updateMovie(movie_id, title, movie_year, movie_rank, genres);
-            	
-            	Movie newMovie = MovieLogicImpl.returnMovieViaID(movie_id);
-            	ArrayList < Review > reviews = new ArrayList < Review > ();
-            	reviews = MovieLogicImpl.returnReviews(newMovie);
-            	
-            	ArrayList < String> newGenres = MovieLogicImpl.getGenreOfMovie(newMovie);
-            	
-            	root.put("genres", newGenres);
-
-            	root.put("movie", newMovie);
-            	root.put("reviews", reviews);
-            	templateName = "reviews.ftl";
-        	}
-        	
-        	else
-        	{
-           	
-        		Movie movie = MovieLogicImpl.returnMovieViaID(movie_id);
-        		
-            	String Error = "You must have fill out the correct fields";
-            	
-            	root.put("error", Error);
-            	root.put("movie", movie);
-            	templateName = "manageMovie.ftl";
-        	}
- 
-        
-        }
-        
-        /** 
-         * is active when a user wants to delete a movie
-         */
-        
-        if(request.getParameterMap().containsKey("deleteMovie"))
-        {
-        	String id = request.getParameter("movieID");
-        	int movie_id = Integer.parseInt(id);
-        	
-        	MovieLogicImpl.deleteMovie(movie_id);
-        	
-        	templateName = "deleteSuccess.ftl";
-        
-        }
-        
-
         try
         {
             
@@ -419,18 +76,58 @@ public class FreelancerBoundary extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        try
-        {
-            runTemplate(request, response);
+    	String button = request.getParameter("button");
+        if(button.equals("Login!")){
+			try {
+				runWelcome(request,response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		
+		}
+        else if(button.equals("Sign Up!")){ 
+        	signUp(request,response); 
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
+        
+        else if(button.equals("My Profile")){ 
+        	myProfile(request,response);
         }
-
+        else if(button.equals("Sign Out")){
+        	signOut(request,response); 
+        }
+        else if(button.equals("Submit Task")){ 
+        	submitTask(request,response); 
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    private void submitTask(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void myProfile(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void signOut(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void signUp(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void runWelcome(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+		
+    	runTemplate(request, response,"welcome");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         doGet(request, response);
     }
