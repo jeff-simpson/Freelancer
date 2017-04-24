e933bccdc86bff0d783e87f238b1288df6a160b7
Ivano Milo <ivanomilo>
2017-04-24 11:15:22 -0400

    smal

--- src/edu/uga/cs4300/boundary/FreelancerBoundary.java
+++ src/edu/uga/cs4300/boundary/FreelancerBoundary.java
@@ -228,7 +228,7 @@ public class FreelancerBoundary extends HttpServlet
 			} 
         }
         
-        else if(button.equals("Offer Your Services")){ 
+        else if(button.equals("Request")){ 
         	
         	try {
 				offerServices(request,response);
