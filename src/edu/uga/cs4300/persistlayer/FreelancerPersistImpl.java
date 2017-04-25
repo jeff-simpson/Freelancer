61b4aa26c47c3c5e14775f1cc71d137de2886d50
Jeff Simpson <jeffsimpson199@gmail.com>
2017-04-24 23:33:13 -0400

    update query

--- src/edu/uga/cs4300/persistlayer/FreelancerPersistImpl.java
+++ src/edu/uga/cs4300/persistlayer/FreelancerPersistImpl.java
@@ -157,7 +157,7 @@ public class FreelancerPersistImpl {
 	public static ResultSet returnPastTaskByUser(User user) throws SQLException
 	{
 		
-		String query = "SELECT * FROM tasks, taskStatus "+
+		String query = "SELECT tasks.id, tasks.description, tasks.time, tasks.price, tasks.difficulty, tasks.user_id, tasks.location FROM tasks, taskStatus "+
 						"WHERE tasks.id = taskStatus.task_id and taskStatus.status = 'closed' and user_id ='"+user.getId()+"';";
 		
 		ResultSet rs = DbAccessInterface.retrieve(query);
